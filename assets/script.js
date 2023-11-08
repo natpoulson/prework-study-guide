5e32989238c444354b3ccea62de3b129731deca0
Nat Poulson <nat.poulson@outlook.com.au>
2023-11-08 21:27:14 +1100

    refactor js code to include functions and for loops, add JS notes to prework

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
-// const topic = "JavaScript";
 const topics = ["HTML", "CSS", "Git", "JavaScript"];
 
-// if (topics.includes(topic)) {
-//     console.log(`Let's learn ${topic}!`);
-// } else {
-//     console.log("Please try again!");
-// }
-
-for (const topic of topics) {
-    console.log(topic);
+function listTopics() {
+    for (const topic of topics) {
+        console.log(topic);
+    }
 }
+
+function selectTopic() {
+    const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+    console.log(`Let's study ${randomTopic}!`);
+}
+
+console.log("Here are the topics we've studied through prework");
+listTopics();
+console.log("Which topic should we study next?");
+selectTopic();
