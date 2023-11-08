e81c3dc6fec78416ffc8e758e4b7cf774fc9faef
Nat Poulson <nat.poulson@outlook.com.au>
2023-11-08 19:50:30 +1100

    add js file, link to project, populate initial script

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// const topic = "JavaScript";
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+// if (topics.includes(topic)) {
+//     console.log(`Let's learn ${topic}!`);
+// } else {
+//     console.log("Please try again!");
+// }
+
+for (const topic of topics) {
+    console.log(topic);
+}
